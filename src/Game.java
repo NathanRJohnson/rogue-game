cab2b3b8f53c9b376c875bb79b21022051919ceb
Nathaniel Johnson <39593484+NathanRJohnson@users.noreply.github.com>
2023-06-10 16:45:45 -0400

    [Z-001] Player Movement (#8)

--- src/Game.java
+++ src/Game.java
@@ -1,78 +1,49 @@
 import entities.Player;
-import generation.Door;
-import generation.Map;
 import generation.Room;
 import processing.core.PApplet;
 import processing.core.PVector;
 import tools.Clock;
 import tools.Constants;
-import tools.Direction;
 
 public class Game extends PApplet{
 
-    Player player;
-    Room r;
-    Clock clock;
-    PApplet gameWindow;
-    int gameState;
+  Player player;
+  Room r;
+  Clock clock;
+  PApplet gameWindow;
+  int gameState;
 
-    public void settings(){
-        size(Constants.WIDTH,Constants.HEIGHT);
-        player = new Player();
-        clock = new Clock();
+  public void settings(){
+    size(Constants.WIDTH,Constants.HEIGHT);
+    player = new Player();
+    clock = new Clock();
 
-        r = new Room(player, clock);
-        gameState = Constants.GAMESTATE_PLAY;
-    }
+    r = new Room(player, clock);
+    gameState = Constants.GAMESTATE_PLAY;
+  }
 
-    public void draw() {
-        gameWindow = this;
-        if (gameState == Constants.GAMESTATE_PLAY) {
-            r.run(gameWindow);
-            player.run(gameWindow);
-            r.applyBoundaries(player);
+  public void draw() {
+    gameWindow = this;
+    r.run(gameWindow);
+    player.run(gameWindow);
+    r.applyBoundaries(player);
+  }
 
-            if (r.getRoomNum() == 11 && r.getEnemies().isEmpty()) gameState = Constants.GAMESTATE_WIN;
+  public void keyPressed(){
+    player.press(key, keyCode);
+  }
 
-            if (player.getHealth() < 0.0) gameState = Constants.GAMESTATE_LOSE;
+  public void keyReleased(){
+    player.release(key, keyCode);
+  }
 
-            for (Door d : r.getDoorsMap().values()) {
-                if (!d.isLocked() && d.isEntered(player)) {
-                    Direction card_dir = d.getDirection();
-                    player.setPosByCompass(card_dir);
-                    r = d.getNextRoom();
-                    r.initRoom(gameWindow);
-                }
-            }
-            clock.run();
-        } else if (gameState == Constants.GAMESTATE_LOSE) {
-            textSize(128);
-            fill(0);
-            text("You Lose!!", 345, Constants.HEIGHT/2 + 40);
-        }
-        else if (gameState == Constants.GAMESTATE_WIN) {
-            textSize(128);
-            fill(255, 255, 0);
-            text("You WIN!!", 345, Constants.HEIGHT/2 + 40);
-        }
+  public void mousePressed() {
+    player.fire(new PVector(mouseX, mouseY));
+  }
 
-    }
-
-    public void keyPressed(){
-        player.press(key);
-    }
-
-    public void keyReleased(){
-        player.release(key);
-    }
-
-    public void mousePressed() {
-        player.fire(new PVector(mouseX, mouseY));
-    }
-
-    public static void main(String[] args) {
-        String[] processingArgs = {"Game"};
-        Game game = new Game();
-        PApplet.runSketch(processingArgs, game);
-    }
+  public static void main(String[] args) {
+    String[] processingArgs = {"Game"};
+    Game game = new Game();
+    PApplet.runSketch(processingArgs, game);
+  }
 }
--- src/entities/Player.java
+++ src/entities/Player.java
@@ -3,149 +3,211 @@ package entities;
 import processing.core.PApplet;
 import processing.core.PVector;
 import projectiles.Launcher;
-import tools.Constants;
-import tools.Direction;
 
-public class Player extends Entity{
-    private boolean[] inputs;
-    private Launcher launcher;
-    private PVector facing;
+public class Player extends Entity {
+  private final int FORWARD = 0;
+  private final int BACKWARD = 1;
+  private final int LEFT = 2;
+  private final int RIGHT = 3;
+  private final int SPRINT = 4;
+  private final int SLIDE = 5;
+  private final int WALK = 6;
+  private final int SPRINT_CHARGE_MAX = 70;
+  private final int SLIDE_CHARGE_MAX = 20;
+  private boolean[] inputs;
+  private Launcher launcher;
+  private PVector facing;
+  private float sprintSpeed, walkSpeed, slideSpeed;
+  private int movementState;
+  private int sprintCharge, slideCharge;
 
-    public Player() {
-        super(500,500, 40, 40);
-        facing = new PVector();
+  public Player() {
+    super(500,500, 40, 40);
+    facing = new PVector();
+    inputs = new boolean[6];
+    launcher = new Launcher(getPos(), 400, 8);
+    launcher.setDamage(150);
+    r = 15;
+    health = 255;
+    walkSpeed = 3;
+    sprintSpeed = 6;
+    slideSpeed = 10;
+    maxspeed = walkSpeed;
+  }
 
-        inputs = new boolean[4];
-        launcher = new Launcher(getPos(), 400, 8);
-        r = 15;
-        launcher.setDamage(150);
-        health = 255;
+  public void run(PApplet p){
+    update(p);
+    display(p);
+  }
+
+  @Override
+  public void update(PApplet pa) {
+    facing = PVector.sub(new PVector(pa.mouseX, pa.mouseY), getPos());
+    addToPos(vel);
+    launcher.update(pa);
+    launcher.setPos(getPos());
+    updateHitBox();
+
+    if (movementState == SLIDE){
+      maxspeed = slideSpeed;
+      slide();
+    } else {
+      move();
+      slideCharge = PApplet.max(SLIDE_CHARGE_MAX, slideCharge + 1);
+      if (movementState == SPRINT) {
+        maxspeed = sprintSpeed;
+        // spend sprint energy
+        sprintCharge = PApplet.max(0, sprintCharge - 1);
+        if (sprintCharge <= 0) { movementState = WALK; inputs[SPRINT] = false; };
+      } else {
+        maxspeed = walkSpeed;
+        // recharge sprint speed
+        sprintCharge = PApplet.min(SPRINT_CHARGE_MAX, sprintCharge + 1);
+      }
     }
 
-    public void run(PApplet p){
-        update(p);
-        display(p);
+    //drag effect
+    if (this.acc.mag() == 0){
+      PVector inv = vel.copy();
+      inv.normalize();
+      inv.mult(-1);
+      inv.mult((float) 0.6);
+      acc.add(inv);
+      
+      // removes creep
+      if (PApplet.abs(vel.mag()) <= 0.3) {
+        vel.mult(0);
+      }
     }
 
-    @Override
-    public void update(PApplet pa) {
-        launcher.update(pa);
-        move();
-        facing = PVector.sub(new PVector(pa.mouseX, pa.mouseY), getPos());
+    vel.add(acc);
+    vel.limit(maxspeed);
+    acc.mult(0);
+  }
 
-        //removes creep
-        if (PApplet.abs(vel.mag())<= 0.1) {
-            vel.mult(0);
-        }
+  private void slide() {
+    if (slideCharge <= 0) {
+      movementState = SPRINT;
+    }
+    PVector direction = vel.copy();
+    direction.normalize();
+    direction.mult(slideSpeed);
+    vel.add(direction);
+    slideCharge = PApplet.max(0, slideCharge - 1);
+  }
 
-        addToPos(vel);
-        launcher.setPos(getPos());
-        updateHitBox();
+  @Override
+  public void display(PApplet pa) {
+    pa.fill(193, 37, 173);
+    float theta = facing.heading() + PApplet.PI / 2;
+    pa.fill(0, 50, 200);
+    pa.stroke(0);
+    pa.pushMatrix();
+    pa.translate(getPos().x + r, getPos().y + r);
+    pa.rotate(theta);
+    pa.beginShape();
+    pa.vertex(0, -r*2);
+    pa.vertex(-r, r*2);
+    pa.vertex(r, r*2);
+    pa.endShape(PApplet.CLOSE);
+    pa.popMatrix();
 
-        //drag effect
-        PVector inv = vel.copy();
-        inv.normalize();
-        inv.mult(-1);
-        inv.mult((float) 0.3);
-        acc.add(inv);
+    pa.fill(0);
+    pa.textSize(16);
+    pa.text("Health", 55, 720);
+    pa.fill(0, 200, 0);
+    pa.rect(55, 730, PApplet.max(health, 0), 35);
+    pa.stroke(1);
+    pa.noFill();
+    pa.rect(55, 730, 255, 35);
+  }
 
-        vel.add(acc);
-        vel.limit(maxspeed);
+  public void fire(PVector target) {
+    launcher.fire(target);
+  }
 
-        acc.mult(0);
+  public Launcher getLauncher() {
+    return launcher;
+  }
+
+  public void move() {
+    if (inputs[FORWARD]) {
+      acc.y = -1;
+    }
+    if (inputs[BACKWARD]) {
+      acc.y = 1;
     }
 
-    @Override
-    public void display(PApplet pa) {
-        pa.fill(193,37,	173);
-        float theta = facing.heading() + PApplet.PI/2;
-        pa.fill(0,50,200);
-        pa.stroke(0);
-        pa.pushMatrix();
-        pa.translate(getPos().x + r, getPos().y + r);
-        pa.rotate(theta);
-        pa.beginShape();
-        pa.vertex(0, -r*2);
-        pa.vertex(-r, r*2);
-        pa.vertex(r, r*2);
-        pa.endShape(PApplet.CLOSE);
-        pa.popMatrix();
-
-        pa.fill(0);
-        pa.textSize(16);
-        pa.text("Health", 55, 720);
-        pa.fill(0, 200, 0);
-        pa.rect(55, 730, PApplet.max(health, 0), 35);
-        pa.stroke(1);
-        pa.noFill();
-        pa.rect(55, 730, 255, 35);
+    if (inputs[LEFT]) {
+      acc.x = -1;
+    }
+    if (inputs[RIGHT]) {
+      acc.x = 1;
+    }
+    if (inputs[SPRINT]) {
+      if (sprintCharge >= SPRINT_CHARGE_MAX - 20) {
+        movementState = SPRINT;
+      } else if (movementState != SPRINT) {
+        inputs[SPRINT] = false;
+      }
+    } else {
+      movementState = WALK;
     }
 
-    public void setPosByCompass(Direction d) {
-        switch (d) {
-            case NORTH:
-                setPos(Constants.WIDTH/2, Constants.HEIGHT - 100);
-                break;
-            case SOUTH:
-                setPos(Constants.WIDTH/2, 100);
-                break;
-            case EAST:
-                setPos(100, Constants.HEIGHT/2);
-                break;
-            case WEST:
-                setPos(Constants.WIDTH - 100, Constants.HEIGHT/2);
-        }
+    if (inputs[SLIDE] && inputs[SPRINT]) {
+      if (movementState == SPRINT && slideCharge >= 0) {
+        movementState = SLIDE;
+        slideCharge = 15;
+      }
+    }
+  }
+
+  public void press(char k, int keyCode) {
+    if (k == 'w' || k == 'W') {
+        inputs[FORWARD] = true;
+    } else if (k == 's' || k == 'S') {
+        inputs[BACKWARD] = true;
     }
 
-    public void fire(PVector target) {
-        launcher.fire(target);
+    if (k == 'a' || k == 'A') {
+        inputs[LEFT] = true;
+    } else if (k == 'd' || k == 'D') {
+        inputs[RIGHT] = true;
     }
 
-    public Launcher getLauncher() {
-        return launcher;
+    if (keyCode == PApplet.SHIFT) {
+        inputs[SPRINT] = true;
     }
 
-    public void press(char k) {
-        if (k == 'w' || k == 'W') {
-            inputs[0] = true;
-        } else if (k == 's' || k == 'S') {
-            inputs[1] = true;
-        }
-
-        if (k == 'a' || k == 'A') {
-            inputs[2] = true;
-        } else if (k == 'd' || k == 'D') {
-            inputs[3] = true;
-        }
+    if (keyCode == PApplet.CONTROL) {
+      inputs[SLIDE] = true;
     }
+  }
 
-    public void move() {
-        if (inputs[0]) {
-            acc.y = -1;
-        }
-        if (inputs[1]) {
-            acc.y = 1;
-        }
+  public void release(char k, int keyCode) {
+      if (k == 'w' || k == 'W') {
+          inputs[FORWARD] = false;
+          acc.y = 0;
+      } else if (k == 's' || k == 'S') {
+          inputs[BACKWARD] = false;
+          acc.y = 0;
+      }
 
-        if (inputs[2]) {
-            acc.x = -1;
-        }
-        if (inputs[3]) {
-            acc.x = 1;
-        }
-    }
+      if (k == 'a' || k == 'A') {
+          inputs[LEFT] = false;
+          acc.x = 0;
 
-    public void release(char k) {
-        if (k == 'w' || k == 'W') {
-            inputs[0] = false;
-        } else if (k == 's' || k == 'S') {
-            inputs[1] = false;
-        }
+      } else if (k == 'd' || k == 'D') {
+          inputs[RIGHT] = false;
+          acc.x = 0;
+      }
 
-        if (k == 'a' || k == 'A') {
-            inputs[2] = false;
-        } else if (k == 'd' || k == 'D') {
-            inputs[3] = false;
-        }
-    }
+      if (keyCode == PApplet.SHIFT) {
+        inputs[SPRINT] = false;
+      }
+
+      if (keyCode == PApplet.CONTROL){
+        inputs[SLIDE] = false;
+      }
+  }
 }
